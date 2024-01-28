ae0b31571037298281ef65457302f76abecf8037
MiroslavDev <miroslavdev@MacBook-Air-Miroslav.local>
2024-01-28 20:03:45 +0200

    +

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 
-function slider2(selector){
+function slider2(selector,next,back,setting){
     
     if(!document.querySelector(selector)){
       throw new Error('Неверный селектор')
@@ -12,22 +12,9 @@ function slider2(selector){
     let listItems=[...list.children]
 
 
-const buttonBack = `
-    <button class='slider__back'>
-        <svg height="128px" id="Layer_1" style="enable-background:new 0 0 128 128;" version="1.1" viewBox="0 0 128 128" width="128px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <g>
-                <line style="fill:none;stroke:#2F3435;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="87.5" x2="40.5" y1="111" y2="64"/>
-                <line style="fill:none;stroke:#2F3435;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="40.5" x2="87.5" y1="64" y2="17"/>
-            </g>
-        </svg>
-     
-    </button>`;
-    
-    const buttonNext = `
-    <button class='slider__next'>
-    <svg height="128px" id="Layer_1" style="enable-background:new 0 0 128 128;" version="1.1" viewBox="0 0 128 128" width="128px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line style="fill:none;stroke:#2F3435;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="40.5" x2="87.5" y1="17" y2="64"/><line style="fill:none;stroke:#2F3435;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="87.5" x2="40.5" y1="64" y2="111"/></g></svg>
-     
-    </button>`;
+const buttonBack = ` <button class='slider__back'>${back}</button>`;
+const buttonNext = `<button class='slider__next'>${next}</button>`;
+
 const currentSlide="<div class='slider__current'></div>"
 
 const slider=`<div class="slider">${buttonBack}${currentSlide}${buttonNext}</div>`
@@ -114,7 +101,17 @@ console.log(currentClass)
     
     }
 
-    slider2('.list')
+
+    const svgBack=` <svg height="128px" id="Layer_1" style="enable-background:new 0 0 128 128;" version="1.1" viewBox="0 0 128 128" width="128px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g>
+        <line style="fill:none;stroke:#2F3435;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="87.5" x2="40.5" y1="111" y2="64"/>
+        <line style="fill:none;stroke:#2F3435;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="40.5" x2="87.5" y1="64" y2="17"/>
+    </g>
+</svg>`
+
+const svgNext=`<svg height="128px" id="Layer_1" style="enable-background:new 0 0 128 128;" version="1.1" viewBox="0 0 128 128" width="128px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line style="fill:none;stroke:#2F3435;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="40.5" x2="87.5" y1="17" y2="64"/><line style="fill:none;stroke:#2F3435;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="87.5" x2="40.5" y1="64" y2="111"/></g></svg>`
+
+    slider2('.list',svgNext,svgBack)
     
     
      
